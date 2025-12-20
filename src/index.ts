dff62ef8fa7ab3daf227f4265c164154d534003b
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-20 13:15:31 -0800

    Custom dialog

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,20 @@
  */
 
 function menuItem1() {
-    SpreadsheetApp.getActiveRange().setValue('Hello world')
+    var html = HtmlService.createHtmlOutputFromFile('src/ui/index')
+        .setWidth(400)
+        .setHeight(300);
+    SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+        .showModalDialog(html, 'My custom dialog');
 }
 
-function onOpen(event: {authMode: GoogleAppsScript.Script.AuthMode}) {
+function onOpen(event: { authMode: GoogleAppsScript.Script.AuthMode }) {
     console.log(event.authMode)
     const ui = SpreadsheetApp.getUi();
     // Or DocumentApp, SlidesApp or FormApp.
     ui.createMenu('Custom Menu')
         .addItem('First item', 'menuItem1')
-        .addSeparator()
-        .addSubMenu(ui.createMenu('Sub-menu')
-            .addItem('Second item', 'menuItem2'))
-        .addToUi();   
+        .addToUi();
 }
 
 function onInstall(event: GoogleAppsScript.Events.AddonOnInstall) {
